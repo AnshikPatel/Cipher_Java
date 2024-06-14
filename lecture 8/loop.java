c586f4252773c84bdbe1fa1b2e4a06590e961c07
Anshik <119244261+AnshikPatel@users.noreply.github.com>
2024-06-15 00:00:25 +0530

    Add files via upload

---
+++ lecture 8/loop.java
@@ -0,0 +1,23 @@
+public class loop {
+    public static void main(String args[]){
+
+    int i;
+    System.out.println("for loop");
+    for(i=0;i<=5;i++){
+        System.out.println("value of i is  :"+i);
+    }
+    System.out.println("while loop");
+    while(i<=10){
+        System.out.println("value of i is  :"+i);
+        i++;
+    }
+
+    System.out.println("doo while loop");
+
+    do{
+        System.out.println("value of i is  :"+i);
+        i++;
+    }
+    while(i<=15);
+    }
+}
